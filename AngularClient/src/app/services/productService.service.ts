6ab31138f2928eeaa6637e691c684a339899ddfe
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-12 13:48:38 +0500

    Add getAllProducts method to ProductService

--- AngularClient/src/app/services/productService.service.ts
+++ AngularClient/src/app/services/productService.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { ProductRequest, ProductRespone } from "../modules/product.model";
 import { Injectable } from '@angular/core';
 import { AppModule } from "../app.module";
+import { catchError } from "rxjs/internal/operators/catchError";
 
 @Injectable({
     providedIn: AppModule
@@ -14,6 +15,16 @@ export class ProductService {
     getProducts() {
         return this.http.get<ProductRespone[]>(this.apiAddress+"GetAllProduct");
     }
+    getAllProducts() {
+        return this.http.get<ProductRespone[]>(this.apiAddress + 'GetAllProduct')
+            .pipe(
+                catchError((error: any) => {
+                    console.error('Error fetching products:', error);
+                    throw error;
+                })
+            );
+    }
+
     getProduct(id: number) {
         return this.http.get<ProductRespone>(this.apiAddress + 'GetProductById/' + id);
     }
