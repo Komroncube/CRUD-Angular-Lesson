276a27665b28b11abf6159bdf5d71e2e01b09d38
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-12 13:48:06 +0500

    Refactor table component to use OnInit interface

--- AngularClient/src/app/components/table/table.component.ts
+++ AngularClient/src/app/components/table/table.component.ts
@@ -1,78 +1,35 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProductRespone } from '../../modules/product.model';
 import { ProductService } from '../../services/productService.service';
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
 
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
-export class TableComponent implements AfterViewInit{
+export class TableComponent implements OnInit{
   displayedColumns: string[] = ['id', 'productName', 'category', 'condition', 'price', 'comment', 'date'];
-  dataSource: MatTableDataSource<ProductRespone>;
+  dataSource!: MatTableDataSource<ProductRespone>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   products!: ProductRespone[];
 
   constructor(private productService: ProductService) {
-    // Create 100 users
-    // const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
 
-    this.productService.getProducts().subscribe((data) => {
-      this.products = data;
-      console.log(this.products);
+  }
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe((response) => {
+      this.products = response;
+      this.dataSource = new MatTableDataSource(this.products);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(this.products);
   }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
+  
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
